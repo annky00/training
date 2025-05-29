47202bff223b10ac4105892101cee1dfb608ad14
Anna Kysil <105375353+annky00@users.noreply.github.com>
2025-05-29 11:37:13 +0300

    Delete sql directory

--- sql/password_resets.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE password_resets (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NOT NULL,
-    expires DATETIME NOT NULL,
-    INDEX(email),
-    INDEX(token)
-);
-
