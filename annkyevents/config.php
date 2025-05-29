9f634a627b56b682faa6ba613629c92991d63455
Anna Kysil <105375353+annky00@users.noreply.github.com>
2025-05-29 11:43:07 +0300

    config.php

--- annkyevents/config.php
+++ annkyevents/config.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $dbname = 'annkyeve_event_management';
 $username = 'annkyeve_annky';
-$password = 'Vehxbr77@';
+$password = ПАРОЛЬ;
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
